2b15214d142cb24182382eaee82cfe50a72258b5
thomas302 <mr.actually12@gmail.com>
2018-06-27 16:54:55 -0700

    Update encode.py

--- encode.py
+++ encode.py
@@ -3,3 +3,11 @@
 Created on Tue Jun 27 13:49:21 2018
 @author: Joel Velez
 """
+class Encode():
+    def replaceBytes(byteList, hexStringList):
+        for i in range(len(hexStringList)):
+            byteList[i] = byteList[i] + hexStringList[i]
+        return byteList
+
+    def encode():
+        
